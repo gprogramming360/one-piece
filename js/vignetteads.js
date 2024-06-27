af2e6fb6390ced816fd02fe911a3c8bd0d9ba4d4
Ksites <158746568+gprogramming360@users.noreply.github.com>
2024-06-27 12:44:16 +0100

    update vignetteads.js

--- js/vignetteads.js
+++ js/vignetteads.js
@@ -1,2 +1,2 @@
-var ads2="<script></script>";
+var ads2="<script>(function(d,z,s){s.src='https://'+d+'/401/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('gloaphoo.net',7658809,document.createElement('script'))</script>";
 document.write(ads2);
