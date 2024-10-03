d8e838238e848549e1e9cf4fb89035994b74ea39
Ksites <158746568+gprogramming360@users.noreply.github.com>
2024-10-03 23:35:46 +0100

    update vignetteads.js

---
+++ vignetteads.js
@@ -0,0 +1,1 @@
+<script>(function(d,z,s){s.src='https://'+d+'/401/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('psulrushe.net',8189104,document.createElement('script'))</script>
