a230e5933aa095dd6124a39bbf1c75a26f9c5b72
Ksites <158746568+gprogramming360@users.noreply.github.com>
2024-10-03 23:12:12 +0100

    update vignetteads.js

--- js/vignetteads.js
+++ js/vignetteads.js
@@ -1,1 +1,1 @@
-    <script>(function(d,z,s){s.src='https://'+d+'/401/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('psulrushe.net',8189104,document.createElement('script'))</script>
+<script>(function(d,z,s){s.src='https://'+d+'/401/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('psulrushe.net',8189104,document.createElement('script'))</script>
